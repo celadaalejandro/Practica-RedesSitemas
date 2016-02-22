3c0e5516de3f987143fbb374808c78c61a40e8bc
celadaalejandro <celadaalejandro@gmail.com>
2016-02-22 14:31:08 +0100

    TCP 1.2
    Solo falta la resolución DNS

--- RedesySistemas/TCP/ping_oc.c
+++ RedesySistemas/TCP/ping_oc.c
@@ -6,7 +6,14 @@
 #include <stdlib.h> //system("clear")
 #include <unistd.h> //sleep()
 #include <sys/time.h>//struct timeval
+#include <signal.h> //  Señal para capturar CTRL+C
 #define SIZE_SEND 32
+int flag_CTRL_C=0;
+
+void control(int sig)
+{
+ 	flag_CTRL_C=1;
+}
 
 int main (int argc, char *argv[])
 {
@@ -16,6 +23,8 @@ int main (int argc, char *argv[])
 		printf("ERROR:USO INCORRECTO DEL PROGRAMA ES:\n\n--->>ping_noc.c IP PUERTO N_REPETICIONES<<---\n\n");
 		exit(1);
 	}
+
+	signal(SIGINT, control);
 	char message[SIZE_SEND];
 	memset((char*)&message, 0, sizeof(message));
 	int fd;
@@ -44,6 +53,7 @@ int main (int argc, char *argv[])
 	int size_data;
 	//Datos para el timer
 	struct timeval t_ini, t_fin;
+	double v_max,v_min;
   	double secs;
   	double media=0;
 	printf("-------->Mandando una rafaga de %d pings a la direción IP %d:%d<--------\n\n",atoi(argv[3]),atoi(argv[1]),atoi(argv[2]));
@@ -52,50 +62,70 @@ int main (int argc, char *argv[])
 	int contador_real=0;
 	do
 	{
-		i++;
-		size_data=sizeof(datos);
-		//Enviamos el mesaje. Si se ha solitado el cirre (0 bytes).
-		if (send(fd, datos,size_data,0)< 0)
+		if(!flag_CTRL_C)
 		{
-			perror("No se ha podido responder al cliente");
-			close(fd);
-			return 0;
-		}
-		else
-		{
-			gettimeofday(&t_ini, NULL);
-			//printf(">>> Se han enviado %d bytes a la IP %s a su puerto %d.\n",size_data,inet_ntoa(server.sin_addr),ntohs(server.sin_port));
-		}
-		//Recibir mensaje del cliente
-		int size_message;
-		if((size_message = recv(fd,datos,sizeof(datos),0))==-1)
-		{
-			perror("No se ha podido recibir los datos del cliente");
-			close(fd);
-			return 0;
-		}
-		else if (size_message==0)
-		{
+			i++;
+			size_data=sizeof(datos);
+			//Enviamos el mesaje. Si se ha solitado el cirre (0 bytes).
+			if (send(fd, datos,size_data,0)< 0)
+			{
+				perror("No se ha podido responder al cliente");
+				close(fd);
+				return 0;
+			}
+			else
+			{
+				gettimeofday(&t_ini, NULL);
+				//printf(">>> Se han enviado %d bytes a la IP %s a su puerto %d.\n",size_data,inet_ntoa(server.sin_addr),ntohs(server.sin_port));
+			}
+			//Recibir mensaje del cliente
+			int size_message;
+			if((size_message = recv(fd,datos,sizeof(datos),0))==-1)
+			{
+				perror("No se ha podido recibir los datos del cliente");
+				close(fd);
+				return 0;
+			}
+			else if (size_message==0)
+			{
 
-			printf("\nCancelada la conexion TCP.\n\n");
-			i=flag;
-		}
-		else
+				printf("\nCancelada la conexion TCP por el servidor. Cerramos el socket\n\n");
+				i=flag;
+				close(fd);
+			}
+			else
+			{
+
+				contador_real++;
+				gettimeofday(&t_fin, NULL);
+				secs =((double)(t_fin.tv_sec + (double)t_fin.tv_usec/1000000)-(double)(t_ini.tv_sec + (double)t_ini.tv_usec/1000000))*1000000;
+				media=media+secs;
+				printf("%d bytes de la IP %s:%d seq=%d RTT:  %f micrisegundos\n",size_data,inet_ntoa(server.sin_addr),ntohs(server.sin_port),i,secs);	
+				if(contador_real==1)
+					v_max=v_min=secs;
+				if(v_max<secs)
+					v_max=secs;
+				if(v_min>secs)
+					v_min=secs;
+
+			}
+			sleep(1);
+		}else
 		{
-			contador_real++;
-			gettimeofday(&t_fin, NULL);
-			secs =((double)(t_fin.tv_sec + (double)t_fin.tv_usec/1000000)-(double)(t_ini.tv_sec + (double)t_ini.tv_usec/1000000))*1000000;
-			media=media+secs;
-			printf("%d bytes de la IP %s: %d seq=%d. RTT:  %f micrisegundos\n",size_data,inet_ntoa(server.sin_addr),ntohs(server.sin_port),i,secs);		}
-		sleep(1);
+				printf("\nCancelada la conexion TCP por el cliente. Cerramos el socket\n\n");
+				i=flag;
+				close(fd);
+
+		}
 	}while(i<flag);
 	//printf("\nEnviamos 0 bytes y cerramos el socket del cliente\n");
 	close(fd);
 	//Estadistica
-	printf("\n-------------------------------------------------------\n");
+	printf("\n-------------------------------------------------------------\n");
 	printf("El RTT de las %d muestras es de  %f microsegundos.\n",contador_real,media/contador_real);
 	printf("Tiempo de rafaga: %f microsegundos.\n",media);
-	printf("------------------------------------- --------------------\n");
+	printf("RTT max: %f microsegundos. RTT min: %f microsegundos.\n",v_max,v_min);
+	printf("------------------------------------- --------------------------\n");
 	return 0;
 }
 
--- RedesySistemas/TCP/ping_oc_serv.c
+++ RedesySistemas/TCP/ping_oc_serv.c
@@ -4,21 +4,31 @@
 #include <unistd.h> //close(),write()
 #include <string.h> //strlen
 #include <stdlib.h> //system("clear")
+#include <signal.h> //  Señal para capturar CTRL+C
 #define CON_MAX 5
 #define SIZE_MAX_RECV 32
 
+int fd;
+
+void control(int sig)
+{
+  	printf("\nCancelada la conexion TCP por el servidor. Cerramos el socket\n");
+	close(fd);
+	exit(-1);
+}
+
 int main(int argc , char * argv[])
 {
+
 	//int socket_desc, new_socket, c;
 	//char client_message[2000], *message;
-
+	signal(SIGINT, control);
 	system("clear");
 	if(argc!=2)
 	{
 		printf("ERROR:USO INCORRECTO DEL PROGRAMA ES: PUERTO\n");
 		exit(1);
 	}
-	int fd;
 	//Creamos el socket
 	if ((fd = socket(PF_INET, SOCK_STREAM, 0)) < 0)		
 	{
@@ -59,47 +69,41 @@ int main(int argc , char * argv[])
 	{
 		printf("Aceptamos la conexion\n");
 	}
-	int flag=0;
 	char datos[SIZE_MAX_RECV];
 	int size_message;
 	while(1)
 	{
 
-		while(!flag)
+		//Recibir mensaje del cliente
+		if((size_message = recv(fd,datos,sizeof(datos),0))==-1)
 		{
-			//Recibir mensaje del cliente
-			if((size_message = recv(fd,datos,sizeof(datos),0))==-1)
-			{
-				perror("No se ha podido recibir los datos del cliente");
-				close(fd);
-				return 0;
-			}
-			else if (size_message==0)
-			{
-				printf("Se ha recibido 0 bytes. Cerramos la conexion TCP.\n\n");
-				close(fd);
-				return 0;
-			}
-			else
-			{
-				printf("+++ Se ha recibido %d  bytes de la IP %s de su puerto %d.\n",size_message,inet_ntoa(client.sin_addr),ntohs(client.sin_port));
-			}
-			//Reenviamos el mesaje si no se ha solitado el cirre (0 bytes), y asi damos paso a la siguiente conexion
-			if (send(fd, datos, size_message,0)< 0)
-			{
-				perror("No se ha podido responder al cliente");
-				close(fd);
-				return 0;
-			}
-			else
-			{
-				printf(">>> Se han enviado %d bytes a la IP %s a su puerto %d.\n",size_message,inet_ntoa(client.sin_addr),ntohs(client.sin_port));
-			}
+			perror("No se ha podido recibir los datos del cliente");
+			close(fd);
+			return 0;
+		}
+		else if (size_message==0)
+		{
+			printf("Cancelada la conexion TCP por el cliente. Cerramos el socket.\n\n");
+			close(fd);
+			return 0;
+		}
+		else
+		{
+			printf("+++ Se ha recibido %d  bytes de la IP %s de su puerto %d.\n",size_message,inet_ntoa(client.sin_addr),ntohs(client.sin_port));
+		}
+		//Reenviamos el mesaje si no se ha solitado el cirre (0 bytes), y asi damos paso a la siguiente conexion
+		if (send(fd, datos, size_message,0)< 0)
+		{
+			perror("No se ha podido responder al cliente");
+			close(fd);
+			return 0;
+		}
+		else
+		{
+			printf(">>> Se han enviado %d bytes a la IP %s a su puerto %d.\n",size_message,inet_ntoa(client.sin_addr),ntohs(client.sin_port));
 		}
 	}
-	printf("\nCerramos el socket del servidor\n");
-	close(fd);
-	return 0;
+
 }
 
 	     
