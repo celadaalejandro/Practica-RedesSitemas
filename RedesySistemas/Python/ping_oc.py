0c5363011dd9b6a0e54a326ba47de164f01bedb3
celadaalejandro <celadaalejandro@gmail.com>
2016-02-24 13:00:16 +0100

    TCP CLIENT 1.2
    Solo falta CTRL+C

--- RedesySistemas/Python/ping_oc.py
+++ RedesySistemas/Python/ping_oc.py
@@ -3,6 +3,9 @@ import socket
 import sys
 import os
 import time
+import time
+
+
 
 
 os.system('clear')
@@ -29,24 +32,44 @@ except socket.error, msg:
 
 print '-------->Mandando una rafaga de'+sys.argv[4]+'pings a la direcion IP '+ host_ip +':'+ sys.argv[2]+'<-----------'
 mensaje = 0
-aux=0
+sumador_rtt = 0
+contador = 0
+v_max = 0
+v_min = 0
+
 for x in xrange(int(sys.argv[3])):
 	mensaje = str(mensaje).zfill(int(sys.argv[4])) 
 	try: 
 		s.send(mensaje)
-		print '>>>Se ha enviado '+ str(len(mensaje)) +' bytes a la IP '+host_ip+':'+sys.argv[2]+'.'
+		time_u = time.clock()
+		#print '>>>Se ha enviado '+ str(len(mensaje)) +' bytes a la IP '+host_ip+':'+sys.argv[2]+'.'
 	except socket.error, msg:
 		print '\nNo se ha podido enviar los datos.\nTipo de error: ' + msg[1]
 		break
 
 	try:
 		mensaje = s.recv((len(mensaje)))
-		print '<<<Se ha recibido '+ str(len(mensaje)) +' bytes de la IP '+ host_ip +':'+ sys.argv[2]+'.'
+		rtt = time.clock() - time_u
+		print '<<<Se ha recibido '+ str(len(mensaje)) +' bytes de la IP '+ host_ip +':'+ sys.argv[2]+'. Seq= '+str(x+1)+' RTT: '+ str(rtt*1000000) +' us'
+		sumador_rtt += rtt
+		contador += 1
+		if contador == 1:
+			v_max = v_min = rtt
+		if v_max < rtt:
+			v_max = rtt
+		if v_min > rtt:
+			v_min = rtt
 	except socket.error, msg:		
 		print '\nNo se ha podido recibir datos o se ha pulsado CTRL+C.\nTipo de error: ' + msg[1]
 		break
 	if len(mensaje) == 0:
 		break
 	time.sleep(1)
-print 'METER ESTADSISTICA'
+	#Estadistica
+
+print '\n\n-------------------------------------------------------------'
+print 'El RTT de las '+str(contador)+' muestras es de '+ str((sumador_rtt/contador)*1000000) +' us.'
+print 'Tiempo de rafaga total es: '+ str(sumador_rtt*1000000) +' us.'
+print 'RTT max: '+str(v_max*1000000)+' us. RTT min: '+str(v_min*1000000)+' us.'
+print '------------------------------------- --------------------------'
 s.close()
